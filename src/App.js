c90d2104a18e9b2c95ebaae974fd7e2816ab0c82
Kangsunggyu <hgu1235@handong.ac.kr>
2024-10-28 10:51:57 +0900

    123

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Component1 from './Component1.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Component1/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
