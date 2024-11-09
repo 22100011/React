776cb616c6fa5324d98d00eb0729d9c12b2c90b5
Kangsunggyu <hgu1235@handong.ac.kr>
2024-11-09 16:07:50 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import Component1 from './Component1.js';
+
+import Component from './Component/component.js';
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Component1/>
+        <Component/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>10주차 과제 css 3개 추가하기</h2>
+        <h3>3개 추가 완료</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Component/component.js
@@ -0,0 +1,9 @@
+import './component.css';
+
+function component(props) {
+  return (
+    <h1>22100011 강성규</h1>
+  );
+}
+
+export default component;
